1ae3dd5e1c2e50c7d46a4fb5deeafb5043de3afc
SiddhantMondal <siddhantmondal9@gmail.com>
2024-03-29 02:05:00 +0530

    a fruits javascript

---
+++ fruits.js
@@ -0,0 +1,17 @@
+
+const header = document.getElementById("header");
+header.style.backgroundColor = "green";
+header.style.color = "orange";
+header.style.borderBottom = '3px solid orange';
+
+const mainHeading = document.getElementById("main-heading"); 
+mainHeading.style.color = "green";
+mainHeading.textContent = 'Fresh fruits Shop';
+
+const basketHeading = document.getElementById("basket-heading");
+basketHeading.style.color = "green"; 
+basketHeading.style.fontStyle = 'italic';
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML = '<p>Please visit us again</p>';
+thanks.style.fontWeight = 'bold';
